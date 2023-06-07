9ec1e11f2d2acca09696d95ccfcff62984528d38
Partho Debnath <parthodebnath28@gmail.com>
2023-06-07 18:58:12 +0600

    Row widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/column_widget.dart';
+import '../widgets/row_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter Widgets'),
       ),
-      body: const ColumnWidget(),
+      body: const RowWidget(),
     );
   }
 }
