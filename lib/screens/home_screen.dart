14cd48e39fc2800ccf2384b7090364af5f1a4183
Partho Debnath <parthodebnath28@gmail.com>
2023-06-07 18:20:27 +0600

    Text Widgets

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/text_and_center.dart';
+import '../widgets/text.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
