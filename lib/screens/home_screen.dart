80337ee282da82e39abfe841e8ef31ef1167c3aa
Partho Debnath <parthodebnath28@gmail.com>
2023-06-08 00:17:08 +0600

    AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,9 +9,39 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        shadowColor: Colors.deepPurple,
+        elevation: 8,
+        forceMaterialTransparency: false,
+        foregroundColor: Colors.black,
+        leading: const Icon(Icons.app_registration),
+        leadingWidth: 56,
+        primary: false,
+        titleSpacing: 100,
+        toolbarHeight: 90,
+        shape: const RoundedRectangleBorder(
+          side: BorderSide(
+            width: 1,
+            color: Colors.red,
+            style: BorderStyle.solid,
+          ),
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
+        titleTextStyle: const TextStyle(
+          color: Colors.green,
+          fontSize: 25,
+        ),
+        centerTitle: true,
         title: const Text('Flutter Widgets'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
-      body: const RowWidget(),
     );
   }
 }
