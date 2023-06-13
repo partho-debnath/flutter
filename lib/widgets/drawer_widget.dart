91dab47c587e825c260b56fda834731bb6f6dd2c
Partho Debnath <parthodebnath28@gmail.com>
2023-06-13 16:22:44 +0600

    Drawer, AppBar

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -5,19 +5,81 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _drawerWithDrawerHeader();
+    return _drawerWithAppBar();
   }
 
-  Drawer _drawerWithDrawerHeader() {
+  Drawer _drawerWithAppBar() {
     return Drawer(
       elevation: 8,
       backgroundColor: Colors.white.withOpacity(0.5),
       child: ListView(
-        // padding: const EdgeInsets.only(left: 10),
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          AppBar(
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(40),
+                child: Image.network(
+                  'https://petapixel.com/assets/uploads/2022/12/what-is-unsplash-800x420.jpg',
+                  fit: BoxFit.cover,
+                  alignment: Alignment.topLeft,
+                ),
+              ),
+            ),
+            title: const Text('Partho Debnath'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  tileColor: Colors.white60,
+                  leading: const Icon(Icons.dashboard_customize),
+                  title: const Text('Dashboard'),
+                  iconColor: Colors.green,
+                  onTap: () {},
+                ),
+                const SizedBox(height: 8.0),
+                ListTile(
+                  tileColor: Colors.white60,
+                  leading: const Icon(Icons.people_alt),
+                  title: const Text('Friends'),
+                  trailing: const Text('50'),
+                  iconColor: Colors.blue,
+                  onTap: () {},
+                ),
+                const SizedBox(height: 8.0),
+                ListTile(
+                  tileColor: Colors.white60,
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Setting'),
+                  iconColor: Colors.red,
+                  onTap: () {},
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
+  Drawer _drawerWithDrawerHeader(BuildContext context) {
+    return Drawer(
+      elevation: 8,
+      backgroundColor: Colors.white.withOpacity(0.5),
+      child: ListView(
         children: <Widget>[
           DrawerHeader(
             padding: EdgeInsets.zero,
+            // margin: EdgeInsets.zero,
             child: UserAccountsDrawerHeader(
               margin: EdgeInsets.zero,
               accountName: const Text('Partho Debnath'),
