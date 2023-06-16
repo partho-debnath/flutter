a27d96c383b7c5dcf6a747fa6fd6b87de27ca03c
Partho Debnath <parthodebnath28@gmail.com>
2023-06-16 12:24:04 +0600

    Expanded

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/layoutbuilder_widget.dart';
+import '../widgets/expanded_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,9 +9,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('LayoutBuilder()'),
+        title: const Text('Expanded()'),
       ),
-      body: const MyLayoutBuilder(),
+      body: const MyExpanded(),
     );
   }
 }
