03f249635b0d71964eee8885c42215e64bca97fe
Partho Debnath <parthodebnath28@gmail.com>
2023-06-27 11:07:14 +0600

    update Chip() widget and add new features.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/materialbanner_widget.dart';
+import '../widgets/chip_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,9 +9,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('showMaterialBanner() Widget'),
+        title: const Text('Chip() Widget'),
       ),
-      body: const MyMaterialBanner(),
+      body: MyChip(),
     );
   }
 }
--- lib/widgets/chip_widget.dart
+++ lib/widgets/chip_widget.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
-class MyChip extends StatelessWidget {
+class MyChip extends StatefulWidget {
   const MyChip({super.key});
 
   @override
+  State<MyChip> createState() => _MyChipState();
+}
+
+class _MyChipState extends State<MyChip> {
+  int index = 0;
+  List<int> list = [1, 2, 3, 4];
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Chip(
+          const Chip(
             label: Text(
               'Hello World',
             ),
           ),
-          Chip(
+          const Chip(
             label: SizedBox(
               width: 800,
               child: Text(
@@ -27,6 +35,39 @@ class MyChip extends StatelessWidget {
               ),
             ),
           ),
+          Chip(
+            backgroundColor: Colors.amber,
+            deleteIconColor: Colors.red,
+            side: const BorderSide(
+              color: Colors.green,
+              width: 2,
+            ),
+            avatar: const CircleAvatar(
+              backgroundColor: Colors.green,
+              child: Text('H'),
+            ),
+            label: const Text(
+              'Hello World',
+            ),
+            deleteIcon: const Icon(Icons.close),
+            onDeleted: () {},
+          ),
+          ...list.map((value) {
+            return Chip(
+              key: ValueKey(value),
+              deleteButtonTooltipMessage: 'Delete This Item',
+              deleteIconColor: Colors.red,
+              avatar: const CircleAvatar(
+                child: Text('H'),
+              ),
+              label: Text('Chip $value'),
+              deleteIcon: const Icon(Icons.close),
+              onDeleted: () {
+                list.remove(value);
+                setState(() {});
+              },
+            );
+          }).toList(),
         ],
       ),
     );
